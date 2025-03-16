8673c212557875c22a7f0f6684990a8088acfb3e
polinachepel <chepel.polina04@mail.ru>
2025-03-16 12:18:58 +0500

    Add files via upload

--- nado2.js
+++ nado2.js
@@ -1,5 +1,3 @@
-<head><meta charset="UTF-8"></head><pre style="caret-color: rgb(0, 0, 0); color: rgb(0, 0, 0); font-style: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: auto; text-align: start; text-indent: 0px; text-transform: none; widows: auto; word-spacing: 0px; -webkit-tap-highlight-color: rgba(26, 26, 26, 0.3); -webkit-text-size-adjust: auto; -webkit-text-stroke-width: 0px; text-decoration: none; overflow-wrap: break-word; white-space: pre-wrap;">  (function () {
-
   const second = 1000,
 
         minute = second * 60,
@@ -88,5 +86,3 @@ button.onclick = function() {
         button.innerHTML = 'узнать';
 
     }
-
-};</pre>
